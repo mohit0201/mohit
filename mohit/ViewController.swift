0b7237e2ea70de9e931156af5c2dd49ff76330ce
student <student@Administrators-Mac-mini.local>
2017-09-13 16:00:32 +0530

    Initial Commit

---
+++ mohit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mohit
+//
+//  Created by student on 13/09/17.
+//  Copyright © 2017 pri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
